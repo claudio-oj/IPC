642a36a120a9bdb3d8d975dd97da6b3bd982d309
Claudio Ortiz J <35052639+claudio-oj@users.noreply.github.com>
2019-05-12 10:29:19 -0400

    Add files via upload

---
+++ diccionarios/diccio_master.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 08:14:06 2019
+
+@author: COJ
+
+Crea diccionario hjson de productos odepa con las caracteristicas de cada producto.
+
+https://stackoverflow.com/questions/7100125/storing-python-dictionaries
+"""
+
+import os
+os.chdir('D:\\Dropbox\\BA\\Clientes\\HSBC\\2 IPC\\modelos_por_producto\\')
+import pandas as pd
+import hjson
+
+
+""" 1  IMPORTA/FORMATEA/LIMPIA/ BASE DE DATOS ODEPA   """
+
+df= pd.read_csv('precios_frutas_hortalizas_odepa.csv',sep="\t",encoding="latin-1",
+                decimal=',')
+df=df.iloc[:,1:]  
+
+# elimina puntos para que pueda evaluar correctamente: puntos como "thousands separator"
+for x in ['Volumen','Preciomínimo','Preciomáximo','Preciopromedio']:
+    df[x]= df[x].apply(lambda x: x.replace('.',''))
+
+# transforma str --> numeric
+for x in ['Volumen','Preciomínimo','Preciomáximo','Preciopromedio']:
+    df[x]= pd.to_numeric(df[x], errors='coerce')
+
+df.Desde= pd.to_datetime(df.Desde,dayfirst=True)
+df.Hasta= pd.to_datetime(df.Hasta,dayfirst=True)
+
+for x in ['Mercado','Variedad','Calidad','Origen','Unidad decomercialización']:
+    print(df[x].value_counts())
+    print()
+
+#%%
+
+
+""" CREA DICCIONARIO"""
+
+dic_grande= {}
+keys1= list(df.Producto.unique()) # tomate, papa, etc...
+keys2= ['Mercado','Variedad','Calidad','Origen','Unidad decomercialización']
+
+# puebla diccionario grande en base a todos los "chicos por producto"
+for i in keys1:    
+    values2= [list(df[x].unique()) for x in keys2]
+    dic_chico = dict(zip(keys2, values2))  
+    dic_grande[i]= dic_chico
+
+#%%
+
+
+""" GUARDA DICCIONARIO """
+
+with open('diccioh.json', 'w') as fp:
+    hjson.dump(dic_grande, fp, sort_keys=True, indent=4)
+
+#%%
+    
+    
+""" LLAMA DICCIONARIO """
+
+with open('hdiccio.json') as fp:
+    data= hjson.loads(fp.read())
+    
+#%%
+    
+    
+
+
+
+
+
